bd5f40cac3c6fd16680369d64b987af4d56f3315
AshwinkumarMG <90339243+AshwinkumarMgandoli@users.noreply.github.com>
2023-10-26 06:16:44 +0000

    #

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,12 @@
+//functions --- > block of code which will perform a particular task
+
+//synatx
+
+ function add(a, b) {        //a and b are parameters
+    return a + b;           //when return nxt line wil not be executed
+                            //when returned should store value in a variable
+}
+
+
+const addition = add(2,3);  //function call  2 and 3 are arguments
+console.log(addition);
